5f60438e7e17bd785c977329a9f3ffdd22df043f
Eric-revollo <39591108+Eric-revollo@users.noreply.github.com>
2021-04-21 16:37:01 -0400

    Update conditional.js

--- Js/conditional.js
+++ Js/conditional.js
@@ -1,7 +1,7 @@
 ///? to assign a variable depending on condition.
 ///let result = condition ? value1 : value2;
-let age = prompt('age?', 18);
-let message = (age < 3  ) ? 'hi tiny' :
+//let age = prompt('age?', 18);
+//let message = (age < 3  ) ? 'hi tiny' :
  (age < 18) ? "need to see an ID" :
  (age < 100) ? "Greetings" :
  "what an unusual age";
